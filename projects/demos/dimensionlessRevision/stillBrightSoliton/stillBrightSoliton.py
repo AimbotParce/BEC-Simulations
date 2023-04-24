3fac6e545523be6fa4e0c9e49ec4f1c7eea35647
AimbotParce <marcparcerisa@gmail.com>
2023-04-24 12:38:08 +0200

    Rerun simulation for stillBrightSoliton on demos as test

--- projects/demos/dimensionlessRevision/stillBrightSoliton/stillBrightSoliton.py
+++ projects/demos/dimensionlessRevision/stillBrightSoliton/stillBrightSoliton.py
@@ -2,11 +2,15 @@ import jax.numpy as jnp
 
 eta = 1
 kappa = 0
-omega = (kappa**2 + eta**2) / 2
 v = kappa
+omega = (kappa**2 + eta**2) / 2
 
 
 def waveFunction(x, t, constants):
+    kappa = constants["kappa"]
+    eta = constants["eta"]
+    v = constants["v"]
+    omega = constants["omega"]
     return eta * jnp.exp(1j * kappa * x - 1j * omega * t) / jnp.cosh(eta * (x - v * t))
 
 
